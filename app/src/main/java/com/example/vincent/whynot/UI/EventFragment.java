cde10c1e3818096a53348dcd708c0462b9f8e86e
taranpreet <taranpreetkohli@yahoo.co.nz>
2015-08-08 12:31:24 +1200

    test
    testing github

--- app/src/main/java/com/example/vincent/whynot/UI/EventFragment.java
+++ app/src/main/java/com/example/vincent/whynot/UI/EventFragment.java
@@ -27,6 +27,8 @@ import com.example.vincent.whynot.UI.dummy.DummyContent;
  */
 public class EventFragment extends Fragment implements AbsListView.OnItemClickListener {
 
+    //test change
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
