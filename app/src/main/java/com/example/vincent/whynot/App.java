38b76e6ae079b2be483721887999544b1992a9a0
shalskar <vincent.tetau@gmail.com>
2015-08-08 12:14:21 +1200

    First commit
    Basic skeleton code

---
+++ app/src/main/java/com/example/vincent/whynot/App.java
@@ -0,0 +1,10 @@
+package com.example.vincent.whynot;
+
+import android.app.Application;
+
+/**
+ * Created by Vincent on 8/08/2015.
+ */
+public class App extends Application{
+
+}
---
+++ app/src/main/java/com/example/vincent/whynot/UI/Event.java
@@ -0,0 +1,8 @@
+package com.example.vincent.whynot.UI;
+
+/**
+ * Created by Vincent on 8/08/2015.
+ */
+public class Event {
+
+}
